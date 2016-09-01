4d65d3bbd4f403446e2eae7059a53f21fd2c06b6
Rafael Levi <rafaellevissa@hotmail.com>
2016-09-01 00:25:58 -0300

    Create desenho.h

---
+++ desenho.h
@@ -0,0 +1,13 @@
+#ifndef DESENHO_H
+#define DESENHO_H
+#include "jarro.h"
+
+class Desenho
+{
+
+public:
+    void Desenhar(Jarro oJarro1, Jarro oJarro2);
+    Desenho(Jarro oJarro1, Jarro oJarro2);
+};
+
+#endif // DESENHO_H
